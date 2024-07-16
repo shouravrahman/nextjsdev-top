108f6cd99fa4eff62ba655a2303bc7c24e90b91c
shourav <shouravrahman006@gmail.com>
2024-07-17 04:13:19 +0600

    sanity

---
+++ env.ts
@@ -0,0 +1,30 @@
+import { config } from "dotenv";
+config();
+
+export const apiVersion = process.env.SANITY_API_VERSION || "2023-08-18";
+
+export const dataset = assertValue(
+    process.env.SANITY_STUDIO_DATASET || "production",
+    "Missing environment variable: SANITY_STUDIO_DATASET"
+);
+
+export const projectId = assertValue(
+    process.env.SANITY_STUDIO_SANITY_PROJECT_ID,
+    "Missing environment variable: SANITY_STUDIO_SANITY_PROJECT_ID"
+);
+
+const token = `${process.env.SANITY_API_READ_TOKEN}`;
+export const previewToken = assertValue(
+    token,
+    "Missing environment variable:SANITY_API_READ_TOKEN"
+);
+
+export const useCdn = false;
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+    if (v === undefined) {
+        throw new Error(errorMessage);
+    }
+
+    return v;
+}
