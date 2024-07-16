c23902dd14b30da7e6ca953068add9507be8a7f5
shourav <shouravrahman006@gmail.com>
2024-07-17 04:13:50 +0600

    env hack

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,27 +1,27 @@
-
-
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-08-18'
+export const apiVersion = process.env.SANITY_API_VERSION || "2023-08-18";
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+    process.env.SANITY_STUDIO_DATASET || "production",
+    "Missing environment variable: SANITY_STUDIO_DATASET"
+);
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+    process.env.SANITY_STUDIO_PROJECT_ID,
+    "Missing environment variable: SANITY_STUDIO_PROJECT_ID"
+);
 
-const token=`${process.env.SANITY_API_READ_TOKEN}`
-export const previewToken= assertValue(token,'Missing environment variable:SANITY_API_READ_TOKEN')
+const token = `${process.env.SANITY_API_READ_TOKEN}`;
+export const previewToken = assertValue(
+    token,
+    "Missing environment variable:SANITY_API_READ_TOKEN"
+);
 
-export const useCdn = false
+export const useCdn = false;
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage)
-  }
+    if (v === undefined) {
+        throw new Error(errorMessage);
+    }
 
-  return v
+    return v;
 }
